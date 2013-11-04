80220f6806e8a9845110b99da8ad381f8c697e24
maxisi <isi.maxi@gmail.com>
2013-11-04 19:40:35 +0000

    debugging logs

--- process.py
+++ process.py
@@ -26,7 +26,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
-                    filename='/temp/process.log',
+                    filename='temp/process.log',
                     filemode='w')
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
